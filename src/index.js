edf848086d8c9205c47748e7285b4a1f228ae31b
Shahzaib Khan <shahzaibkhanisme@gmail.com>
2024-01-11 20:03:41 -0800

    create first module, containing the logic for creating one To-do item along with several styling attributes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import './todoTask';
 
 
 
-import {loadTodo} from './todoTask';
+import {loadTask} from './todoTask';
 
 const header = document.getElementById('header');
 const mainSection = document.getElementById('mainSection');
 const sidebar = document.getElementById('sidebar')
-const newTask = document.createElement('button');
-newTask.innerText = 'new To-do';
-sidebar.appendChild(newTask);
 
+const todoButton = document.createElement('button');
+todoButton.innerText = 'new To-do';
+sidebar.appendChild(todoButton);
+todoButton.addEventListener('click', () => {
+    //clearDisplay()
+    loadTask();
+})
+
+
+
+
+
+// function clearDisplay() {
+//     while(task.firstChild) {
+//         task.removeChild(task.firstChild);
+//     }
+// }
--- src/todoTask.js
+++ src/todoTask.js
@@ -1,4 +1,37 @@
-export function loadTodo() {
+ export function loadTask() {
 
-    const task = document.getElementById('mainSection');
+    const task = document.createElement('div');
+    task.setAttribute('id', 'taskDiv');
+    document.getElementById('mainSection').appendChild(task);
+
+    const taskDiv = document.createElement('div');
+    taskDiv.textContent = 'To-Do';
+    task.appendChild(taskDiv);
+
+    const taskName = document.createElement('input');
+    taskName.type = 'text';
+    taskName.setAttribute('placeholder', 'task name');
+    taskName.setAttribute('maxLength', '15');
+    taskName.className = 'taskInput';
+    task.appendChild(taskName);
+
+    const taskDescription = document.createElement('textarea');
+    taskDescription.type = 'text';
+    taskDescription.setAttribute('placeholder', 'task details');
+    taskDescription.setAttribute('maxLength', '100');
+    taskDescription.className = 'taskInputDescription';
+    task.appendChild(taskDescription);
+
+    const taskDueDate = document.createElement('input');
+    taskDueDate.setAttribute('type', 'date');
+    taskDueDate.className = 'taskInput';
+    task.appendChild(taskDueDate);
+
+    const taskPriority = document.createElement('input');
+    taskPriority.type = 'text';
+    taskPriority.setAttribute('placeholder', 'how urgent is it?');
+    taskPriority.setAttribute('maxLength', '15');
+    taskPriority.className = 'taskInput';
+    task.appendChild(taskPriority);
+    
 }
